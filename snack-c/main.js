c646b8d105bea3b08b7eec205b2cfe5aa16cf9de
gianluigi7 <gianluigiferraioli7@gmail.com>
2024-05-08 19:27:55 +0200

    snack-c

---
+++ snack-c/main.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const input = [1, 2, 'cavallo', 'cr7'];
+const inputDue = ['trattore', 19, 'alfredobarbiere'];
+
+
+
+function stampa(arrayInput, sommaStringhe) {
+
+    for (let i = 0; i < arrayInput.length; i++) {
+        
+        sommaStringhe += arrayInput[i] + ',';
+    }
+ 
+    return sommaStringhe;
+}
+
+console.log(stampa(input))
