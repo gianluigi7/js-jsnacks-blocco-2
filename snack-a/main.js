7739e568420e7823ec378290810875f20a3454ea
gianluigi7 <gianluigiferraioli7@gmail.com>
2024-05-08 17:11:46 +0200

    first commit

---
+++ snack-a/main.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const inputArray = [];
+
+function ArrayLength(lunghezza) {
+ return console.log(lunghezza.length) 
+}
+
+inputArray.push('1');
+ArrayLength(inputArray);
---
+++ snack4/main.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const arrayUno = [1, 2, 3, 4, 5, 6];
+const arrayDue = [270];
+
+
+while (arrayDue.length < arrayUno.lenght) {
+
+    arrayDue.push(10);
+    
+
+}
+
+console.log(arrayDue);
+console.log(arrayUno);
