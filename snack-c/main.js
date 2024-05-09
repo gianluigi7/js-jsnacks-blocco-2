afb2e0624a09daa6271273f684a96535de080b6c
gianluigi7 <gianluigiferraioli7@gmail.com>
2024-05-09 11:00:46 +0200

    snack-c

--- snack-c/main.js
+++ snack-c/main.js
@@ -6,13 +6,15 @@ const inputDue = ['trattore', 19, 'alfredobarbiere'];
 
 
 function stampa(arrayInput) {
-  let sommaStringhe ='undefined';
+  let sommaStringhe ='';
     for (let i = 0; i < arrayInput.length; i++) {
         
         sommaStringhe += arrayInput[i] + ',';
-    }
  
-    return sommaStringhe;
+
+        return sommaStringhe;
+        
+      }
 }
 
 console.log(stampa(input))
